b35a1d9f3b731c3aa30d2b0633cad1358569873f
Pankaj Wakchaure <pankaj.corp@gmail.com>
2021-06-22 16:10:51 +0000

    Separate configs added for dev and prod.

---
+++ webpack.dev.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: { main: "./src/index.js" },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "",
+    clean: true
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    contentBase: path.resolve(__dirname, "./dist"),
+    index: "index.html",
+    port: 8080,
+    writeToDisk: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,  //extracts css file from bundle.js into separate css file
+          "css-loader"  //reads the contents of css file and returns js representation
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource', //similar to file-loader
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      inject: true
+    })
+  ]
+}
---
+++ webpack.prod.config.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  mode: "production",
+  entry: { main: "./src/index.js" },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "",
+    clean: true
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    contentBase: path.resolve(__dirname, "./dist"),
+    index: "index.html",
+    port: 9090,
+    writeToDisk: true
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        reactMain: {
+          test: /[\\/]node_modules[\\/](react)[\\/]/,
+          name: 'reactMain',
+          chunks: 'all',
+        },
+        reactDom: {
+          test: /[\\/]node_modules[\\/](react-dom)[\\/]/,
+          name: 'reactDom',
+          chunks: 'all',
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,  //extracts css file from bundle.js into separate css file
+          "css-loader"  //reads the contents of css file and returns js representation
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource', //similar to file-loader
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      inject: true
+    }),
+    new BundleAnalyzerPlugin()
+  ]
+}
