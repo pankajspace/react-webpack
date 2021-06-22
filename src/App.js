90d33c367d5c7c185c8314703a976f52bdd6a61e
Pankaj Wakchaure <pankaj.corp@gmail.com>
2021-06-22 14:26:22 +0000

    Button and Text components added, code splitting added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import "./App.css";
+import Button from "./components/Button";
 
 export default function App() {
   return (
     <div className="app">
-      App
+      <Button />
     </div>
   )
 }
